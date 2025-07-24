9900cca0b1bc778e18892360dcd4f494e7b255c9
esmaelehab <63879362+esmaelehab@users.noreply.github.com>
2025-07-24 04:12:32 +0300

    Add project files.

---
+++ assignment6/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace assignment6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region q1
+
+            #endregion
+        }
+    }
+}
