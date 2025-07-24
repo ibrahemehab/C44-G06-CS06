06b7ee5b34943af9c555358f05ce0d43b51e8683
esmaelehab <63879362+esmaelehab@users.noreply.github.com>
2025-07-24 10:43:36 +0300

    question 3

--- assignment6/Program.cs
+++ assignment6/Program.cs
@@ -1,42 +1,54 @@
 ﻿using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
+using System.Transactions;
 
 namespace assignment6
 {
     internal class Program
     {
         #region sum
-        public static int sum(int x, int y)
-        {
-            x = 5;
-            y = 6; ;
-            int z = x + y;
-            return z;
-        }
+        //public static int sum(int x, int y)
+        //{
+        //    x = 5;
+        //    y = 6; ;
+        //    int z = x + y;
+        //    return z;
+        //}
         #endregion
         #region sum2
-        public static int sum2(ref int x, ref int y)
-        {
-            x = 5;
-            y = 6; ;
-            int z = x + y;
-            return z;
-        } 
+        //public static int sum2(ref int x, ref int y)
+        //{
+        //    x = 5;
+        //    y = 6; ;
+        //    int z = x + y;
+        //    return z;
+        //}
         #endregion
-        public static void arr( int[ ]numbers )
-        {
-            numbers[0] = 40;
-            numbers = new int[3];
-            numbers[0] = 60 ;
+        #region arr
+        //public static void arr(int[] numbers)
+        //{
+        //    numbers[0] = 40;
+        //    numbers = new int[3];
+        //    numbers[0] = 60;
 
-        }
-        public static void arr2( ref int[] numbers)
-        {
-            numbers[0] = 40;
-            numbers = new int[3];
-            numbers[0] = 60;
+        //} 
+        #endregion
+        #region arr2
+        //public static void arr2(ref int[] numbers)
+        //{
+        //    numbers[0] = 40;
+        //    numbers = new int[3];
+        //    numbers[0] = 60;
 
+        //}
+        #endregion
+        #region summ,substract
+        public static void  ss ( int x , int y , int z , int h , out int sum , out int substract)
+        {
+            sum = x + y;
+            substract = z - h;
         }
+        #endregion
         static void Main(string[] args)
         {
             #region q1
@@ -65,12 +77,28 @@ namespace assignment6
             //but if you reasssign the the refrence in the method to another object only it will point it so after that changes will not affect the caller
             ////answer passing reference by reference : take the reference it self of the original object so any change happen in the method affect the original 
             //and if you reasssign the the refrence in the method to another object both it and the caller will point to that object so any changes happen after that will affect the caller 
-            int[] seq = [10, 20, 30];
-            arr( seq);
-            Console.WriteLine(seq[0]);
-            int[] seq2 = [10, 20, 30];
-            arr2( ref seq2);
-            Console.WriteLine(seq2[0]);
+            //int[] seq = [10, 20, 30];
+            //arr( seq);
+            //Console.WriteLine(seq[0]);
+            //int[] seq2 = [10, 20, 30];
+            //arr2( ref seq2);
+            //Console.WriteLine(seq2[0]);
+
+
+            #endregion
+            #region q3
+            //Write a c# Function that accept 4 parameters from user and
+            //return result of summation and subtracting of two numbers
+            Console.WriteLine("enter the four numbers : ");
+            int a = int.Parse (Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            int sum;
+            int substract;
+            ss(a, b, c, d, out sum, out substract);
+            Console.WriteLine(sum + "  " + substract);
+
 
 
             #endregion
